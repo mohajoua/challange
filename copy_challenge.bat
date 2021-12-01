0f7d05489563283bec77231e903b4b8ae16af6c9
mohajoua <mohamed.jouamaa@gmail.com>
2021-12-01 14:23:46 -0500

    Init projec
    Init projec

---
+++ copy_challenge.bat
@@ -0,0 +1,4 @@
+:: This file might be use during the interview, no need to review it, just skip it.
+
+robocopy ./frontend ./frontend-backup /E /XD node_modules
+robocopy ./backend ./backend-backup /E
---
+++ copy_challenge.sh
@@ -0,0 +1,6 @@
+# This file might be use during the interview, no need to review it, just skip it.
+
+mv ./frontend/node_modules ./node_modules
+cp -r ./frontend ./frontend-backup;
+cp -r ./backend ./backend-backup
+mv ./node_modules ./frontend/node_modules 
